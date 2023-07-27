c8eecc5a45d980c42a96d3977b06f8741397a894
adamE38 <akwork63@gmail.com>
2023-07-27 21:22:48 +0200

    completed task, but something was wrong, so did another one

---
+++ 021/js/app.js
@@ -0,0 +1,22 @@
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// function getDivisorsCnt(n){
+//     let count = 0
+//     for(let i = 1; i <= n; i++){
+//         if(n % i === 0){
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+// console.log(getDivisorsCnt(4));
+
+// fuck something is wrong here, let's do a different one
+
+// Write a function to split a string and convert it into an array of words.
+
+
+const stringToArray = str => str.split(' ')
